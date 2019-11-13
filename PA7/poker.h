58206bcd44459863ce2edda8e357bcda892c150e
Nathan Brown <nathan.g.brown@wsu.edu>
2019-11-13 09:46:14 -0800

    Starting code broken into three files

---
+++ PA7/poker.h
@@ -0,0 +1,9 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char* wFace[], const char* wSuit[]);
